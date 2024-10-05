b59151ae412fe607c403c9e97492dd2a7b4caf63
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-10-06 05:57:14 +0700

    [FIX] Mengganti Kode Regresi Linear Using BackwardSubs

--- Soal 2/regres_lin_withQ.m
+++ Soal 2/regres_lin_withQ.m
@@ -1,7 +1,13 @@
-function [x,res] = regres_lin_withQ(Q,R,b)
-  [m, n] = size(R);
-  bt = Q' * b;
-  x = R\bt;
-  res = norm(bt(n+1:m));
-end
+function [x, res] = regres_lin_withQ(Q, R, b)
+    [m, n] = size(R);
+    bt = Q' * b;
 
+    % Backward substitution to solve Rx = bt
+    x = zeros(n, 1);
+    for i = n:-1:1
+        x(i) = (bt(i) - R(i, i+1:n) * x(i+1:n)) / R(i, i);
+    end
+
+    % Calculate residual
+    res = norm(bt(n+1:m));
+end
--- Soal 2/regres_lin_woQ.m
+++ Soal 2/regres_lin_woQ.m
@@ -1,5 +1,12 @@
-function [x,res] = regres_lin_woQ(R,bt)
-  [m,n] = size(R);
-  x = R\bt;
-  res = norm(bt(n+1:m));
+function [x, res] = regres_lin_woQ(R, bt)
+    [m, n] = size(R);
+
+    % Backward substitution to solve Rx = bt
+    x = zeros(n, 1);
+    for i = n:-1:1
+        x(i) = (bt(i) - R(i, i+1:n) * x(i+1:n)) / R(i, i);
+    end
+
+    % Calculate residual
+    res = norm(bt(n+1:m));
 end
