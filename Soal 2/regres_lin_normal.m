31d812e03459230cf7d650e1fe82393ea74eb001
pershyfon <108519083+pershyfon@users.noreply.github.com>
2024-10-05 22:29:50 +0700

    [FEAT] Modified linear regression with normal equation code

--- Soal 2/regres_lin_normal.m
+++ Soal 2/regres_lin_normal.m
@@ -1,11 +1,13 @@
-function [X, res] = regres_lin_normal(Z, b)
-    % Menambahkan kolom satu untuk bias (intercept) pada Z
+function [X, res] = regres_lin_LU(Z, b)
     Z_bias = [ones(size(Z, 1), 1), Z];
 
-    % Menghitung theta menggunakan Persamaan Normal
-    X_raw = (Z_bias' * Z_bias) \ (Z_bias' * b);
-    X = X_raw(2:end, :);
+    A = Z_bias' * Z_bias;
+    B = Z_bias' * b;
 
-    % Menghitung residual error
+    [L, U] = LUFact(A);
+    y = ForSubs(L, B);
+    X_raw = BackSubs(U, y);
+
+    X = X_raw(2:end, :);
     res = norm(b - (Z*X), 2);
 end
